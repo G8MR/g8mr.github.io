23c9da2deebcf1fcaea0e59cd9653deeb29dd7f0
G8MR <87982540+G8MR@users.noreply.github.com>
2022-10-24 13:32:36 +0530

    Delete Home directory

--- Home/script.js
+++
@@ -1,6 +0,0 @@
-let fetchRes = fetch(
-"https://scratchdb.lefty.one/v3/user/info/AwesomeYoungCoder");
-        fetchRes.then(res =>
-            res.json()).then(d => {
-                console.log(d)
-            })
